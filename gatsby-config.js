baf3f6cd78f4d46403267ffa061707ebb5280814
Phoenix <drake.mcgarrah@gmail.com>
2020-10-14 11:30:04 -0500

    Added styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Drake's Portfolio`,
+    description: `A portfolio of my coding projects.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -15,11 +15,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Drake's Portfolio`,
+        short_name: `Portfolio`,
         start_url: `/`,
         // This path is relative to the root of the site.
       },
