9727da14767412ed100b7ea5adc5242eb32c1498
Phoenix <drake.mcgarrah@gmail.com>
2020-10-14 11:23:33 -0500

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Portfolio
-
-Deployed: https://phoenix-99hp.github.io/
-
-A collection of my coding projects made using Gatsby.
-
-![](./src/images/code.png)
--- src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/Layout/Layout.jsx"
-
-const NotFoundPage = () => (
-  <Layout>
-  </Layout>
-)
-
-export default NotFoundPage
